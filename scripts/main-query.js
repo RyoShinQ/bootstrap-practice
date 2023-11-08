01b52f0c0f543bc02fc035b93dadb9030cad5fe5
RyoShinQ <syn1201144@gmail.com>
2023-11-08 18:45:11 +0000

    Jquery to update the elements

---
+++ scripts/main-query.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#d1_op1").click(function(){
+        $("#d1_card_img").attr("src", "imgs/img1_1.png");
+        $("#d1_card_img").attr("alt", "This is the example image for option 1");
+        $("#d1_card_title").text("Option 1");
+        $("#d1_card_context").text("Introduction to option 1. Some quick example text to build on the card title and make up the bulk of the card's content.");
+        $("#d1_card_detail").attr("data-bs-target", "#d1_op1_detail_modal")
+    });
+
+    $("#d1_op2").click(function(){
+        $("#d1_card_img").attr("src", "imgs/img1_2.png");
+        $("#d1_card_img").attr("alt", "This is the example image for option 2");
+        $("#d1_card_title").text("Option 2");
+        $("#d1_card_context").text("Introduction to option 2. Some quick example text to build on the card title and make up the bulk of the card's content.");
+        $("#d1_card_detail").attr("data-bs-target", "#d1_op2_detail_modal")
+    });
+ });
